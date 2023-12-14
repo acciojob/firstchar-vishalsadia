012776cc3103f6d82235cc7050b36bc262dc3acd
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-14 12:52:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  // Remove leading and trailing whitespaces
+  const trimmedStr = str.trim();
+
+  // Check if the trimmed string is not empty
+  if (trimmedStr !== '') {
+    // Return the first character of the trimmed string
+    return trimmedStr.charAt(0);
+  }
+
+  // Return an empty string if the input string is empty or contains only spaces
+  return '';
 }
 
-// Do not change the code below
-
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// Test cases
+console.log(firstChar(' Rosa Parks '));  // Output: 'R'
+console.log(firstChar(' Hello World ')); // Output: 'H'
+console.log(firstChar(' '));              // Output: ''
+console.log(firstChar(''));               // Output: ''
